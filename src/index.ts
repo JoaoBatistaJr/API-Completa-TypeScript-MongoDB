0d4db1bd0936f0b2388d6bf79449499ad14759fb
João Santos <joaob.dev@gmail.com>
2025-03-04 06:02:35 -0300

    Revert "Desktop"

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
 import express from "express";
-import { config } from "dotenv";
-import { GetUsersController } from "./controllers/get-users/get-users";
-import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
-import { MongoClient } from "./database/mongo";
+import {config} from "dotenv";
 
 config();
 
-const main = async () => {
-  const app = express();
+const app = express();
 
-  const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
 
-  await MongoClient.connect();
+app.get("/", (req, res) => {
+  res.send("Hello, World!")
+})
 
-  app.get("/users", async (req, res) => {
-    const mongoGetUsersRepository = new MongoGetUsersRepository();
-    const getUsersController = new GetUsersController(mongoGetUsersRepository);
-
-    const { body, statusCode } = await getUsersController.handle();
-
-    res.send(body).status(statusCode);
-  });
-
-  app.listen(port, () => console.log(`listening on port ${port}!`));
-};
-
-main();
+app.listen(port, () => console.log(`listening on port ${port}!`))
